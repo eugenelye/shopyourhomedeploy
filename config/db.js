199f109b8df5d729b87267f5d77f0f0b56a7eba7
eugenelye <eugenelye91@gmail.com>
2022-08-19 16:00:02 +0800

    make it better

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+const connectDB = async (uri) => {
   try {
-   const conn = await mongoose.connect(process.env.MONGO_URI)
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(uri);
+    console.log("DB Connected");
   } catch (error) {
-    console.log(error);
+    console.error(error.message);
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-require('dotenv').config({path:"./.env"})
-const path = require('path');
+const dotenv = require("dotenv");
+dotenv.config();
+const path = require('path')
 const express = require("express");
-const connectDB = require("./config/db");
-connectDB();
-
 const cors = require("cors");
+const connectDB = require("./config/db.js");
 const users = require("./router/users");
-const Furniture = require("./models/Furniture");
-
 
 
 const app = express();
-
 app.use(cors());
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,14 +19,18 @@ app.use("/users", users);
 
 //Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
+  app.use(express.static(path.join(__dirname, "./client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
+
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
+
+connectDB(process.env.MONGODB_URI);
 
 app.listen(PORT, () => {
   console.log(`server started at ${PORT}`);
 });
+
