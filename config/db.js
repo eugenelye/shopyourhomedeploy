f2024576409be4d34b5e298c389e40f368e44174
eugenelye <eugenelye91@gmail.com>
2022-08-19 16:20:44 +0800

    make it better

--- config/db.js
+++ config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://eugenelye:eugene546933@shopyourhome.jgaln0l.mongodb.net/shopyourhome?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://eugenelye:eugene546933@shopyourhome.jgaln0l.mongodb.net/shopyourhome?retryWrites=true&w=majority" , {useNewUrlParser: true});
+    mongoose.connection.once('open', function(){
+    console.log('Conection has been made!');
+        }).on('error', function(error){
+     console.log('Error is: ', error);
+      });
     console.log("DB Connected");
   } catch (error) {
     console.error(error.message);
